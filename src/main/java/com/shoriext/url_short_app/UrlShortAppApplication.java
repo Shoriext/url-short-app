27bdc1a9643df2cc3b541c49c4a544ecf6e5d81e
Samadar1 <urapol18@gmail.com>
2025-11-22 14:11:47 +0300

    Add: start arch

---
+++ src/main/java/com/shoriext/url_short_app/UrlShortAppApplication.java
@@ -0,0 +1,13 @@
+package com.shoriext.url_short_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoriext/url_short_app/UrlShortAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoriext.url_short_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
